e7137dca60e94219a846a8ef571a850795d83889
tsabkuro <55861074+tsabkuro@users.noreply.github.com>
2022-05-17 20:31:11 -0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+// Remove burger when clicked on
+function burgerToggle(x) {
+  x.classList.toggle("burger-toggle");
+}
+
+// let i=0;
+// let images=[];
+// let time=3000;
+
+// images[0] = '../images/LOHSBanner.png';
+// images[1] = '../images/LOHSBanner2.png';
+// images[2] = '../images/LOHSBanner3.png';
+
+// function changeImg(){
+//   document.slide.src = images[i]
+
+//   if(i < images.length - 1){
+//     i++;
+//   } else {
+//     i=0;
+//   }
+
+//   setTimeout('changeImg()', time);
+// }
+
+// window.onload = changeImg;
